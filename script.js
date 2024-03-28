c324b3432fe73710d36d6fb98807de04e1a403e6
HeojeongYeong <68364002+phphll@users.noreply.github.com>
2024-03-28 14:27:28 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let captchaChecked = false;
+
+function beforeSubmit(event) {
+  
+  if (captchaChecked) {
+    
+  } else {
+    alert('check reCAPTCHA');
+    event.preventDefault();
+  }
+}
+
+function captchaSuccess() {
+  captchaChecked = true;
+}
